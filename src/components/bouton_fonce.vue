458f9e586e0c59d4fc2a6fcc94c068f78cb70db2
Aurélie R <aurelie.runser@edu.univ-fcomte.fr>
2022-10-25 21:19:43 +0200

    création du composant bouton_fonce

---
+++ src/components/bouton_fonce.vue
@@ -0,0 +1,8 @@
+<template>
+    <button class="block w-max rounded bg-secondaire_fonce font-catamaran font-semibold text-tertiaire_claire 
+        px-5 py-2 text-base
+        md:px-8 md:py-5 md:text-lg
+        hover:bg-tonic">
+        <slot/>
+    </button>
+</template>
