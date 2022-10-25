158dd4befb3d044bfc886a0ff56348b054886ff8
Aurélie R <aurelie.runser@edu.univ-fcomte.fr>
2022-10-25 17:24:19 +0200

    ajout des couleurs, fonts, tailles personnalisées

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,39 @@ const formKitTailwind = require("@formkit/themes/tailwindcss");
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+        colors: {
+         "fond": "#fafafa",
+         "tonic": "#bc8748",
+         "secondaire_claire": "#f2e5bb",
+         "secondaire_fonce": "#995d3c",
+         "tertiaire_fonce": "#1f1e26",
+         "tertiaire_claire": "#f6f6f6"
+        },
+
+        fontSize: {
+         "base": "1.5rem",
+         "lg": "2.25rem",
+         "xl": "4rem",
+         "2xl": "6rem"
+        },
+        
+        fontFamily: {
+         "palatino-linotype": "Palatino Linotype",
+         "catamaran": "Catamaran"
+        },
+
+        backgroundImage: {
+          "cover": "url('/cover.jpg')"
+        }
+    },
   },
+
   plugins: [
     require("@tailwindcss/typography"),
     require("@tailwindcss/forms"),
     formKitTailwind,
     require("@headlessui/tailwindcss"),
   ],
+
 };
