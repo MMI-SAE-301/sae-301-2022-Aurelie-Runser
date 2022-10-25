eb10888f068742429679b661f6546a5d6a660fee
Aurélie R <aurelie.runser@edu.univ-fcomte.fr>
2022-10-25 21:20:21 +0200

    Accueil - ajout de la partie "Personnalisation"

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,17 +1,55 @@
 <template>
-  <section class="pt-40 h-screen bg-cover_phone lg:bg-cover bg-right-bottom">
-    <logo class="h-16 md:h-32 xl:h-40"/>
+  <div>
 
-    <div class="md:w-2/3 my-10 md:my-20">
-      <h2 class="font-palatino-linotype font-bold italic text-lg xl:text-xl 2xl:text-2xl text-secondaire_claire">Le meilleur des cadeaux, une montre unique.</h2>
-      <bouton_clair class="mx-auto my-10 md:my-18">Créer ma montre</bouton_clair>
+    <!--Cover-->
+    <div class="px-5 md:px-20 pt-40 h-screen bg-cover_phone lg:bg-cover bg-right-bottom">
+      <logo class="h-16 md:h-32 xl:h-40"/>
+  
+      <div class="md:w-2/3 my-10 md:my-20">
+        <h2 class="my-0 font-palatino-linotype font-bold italic text-lg xl:text-xl text-secondaire_claire">Le meilleur des cadeaux, une montre unique.</h2>
+        <bouton_clair class="mx-auto my-10 md:my-18">Créer ma montre</bouton_clair>
+      </div>
     </div>
-  </section>
+
+    <!--Personnalisation-->
+    <section>
+      <h2>Nouveau : une Tik●Tak personnalisée pour les fêtes</h2>
+
+      <div class="flex flex-wrap justify-between gap-x-20">
+        <montre_profil class="hidden lg:block w-1/4"/>
+
+        <div class="basis-96 flex-auto flex flex-col gap-y-10">
+          <p>
+            Pour un cadeau vraiment unique et personnel,
+            <span>créer vous-même votre montre Tik●Tak</span>.
+          </p>
+
+          <p>
+            Profité de cette nouvelle offre, à
+            <span>-30%</span>
+            j’usqu’au 31 décembre.
+          </p>
+
+          <p>
+            Soyez
+            <span>livré pour les fêtes de fin d’année</span>
+            si vous commandez avant le 15 décembre.
+          </p>
+
+          <bouton_fonce class="mx-auto">Créer ma montre</bouton_fonce>
+        </div>
+      </div>
+    </section>
+
+  </div>
 </template>
 
 <script setup lang="ts">
 import logo from "@/components/icons/logo.vue"
 
 import bouton_clair from "@/components/bouton_clair.vue"
+import bouton_fonce from "@/components/bouton_fonce.vue"
+
+import montre_profil from "@/components/montre_profil.vue"
 
 </script>
