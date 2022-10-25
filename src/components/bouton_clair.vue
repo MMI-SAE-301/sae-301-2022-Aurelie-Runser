976af6dc47d959a87f2d1ee4a295afc9aab385d3
Aurélie R <aurelie.runser@edu.univ-fcomte.fr>
2022-10-25 20:08:06 +0200

    création du composant bouton_clair

---
+++ src/components/bouton_clair.vue
@@ -0,0 +1,8 @@
+<template>
+    <button class="block rounded bg-secondaire_claire font-catamaran font-semibold text-tertiaire_fonce 
+        px-5 py-2 text-base
+        md:px-8 md:py-5 md:text-lg
+        hover:bg-tonic">
+        <slot/>
+    </button>
+</template>
