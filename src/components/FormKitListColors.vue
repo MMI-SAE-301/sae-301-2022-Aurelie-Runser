d2fdee34f53346b2560e46064df0cc80d22dd4e8
Aurélie R <aurelie.runser@edu.univ-fcomte.fr>
2022-10-26 13:42:04 +0200

    création FormKitListColors

---
+++ src/components/FormKitListColors.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+
+import { colors } from "@/types";
+
+defineProps<{
+  name?: string;
+  label?: string;
+}>();
+
+</script>
+
+<template>
+    <div>
+        <FormKit
+                :name="name"
+                :label="label"    
+                value="#FFFFFF"
+                type="radio"
+                :options="colors"
+                :sections-schema="{
+                    inner: { $el: null},
+                    decorator: { $el: null},
+                }"
+                input-class="peer sr-only"                
+                options-class="my-3 flex gap-4"
+                outer-class="my-10"
+                legend-class="font-catamaran font-semibold text-lg text-secondaire_fonce"
+                >
+
+                <template #label="context">
+                    <div class="my-1 h-8 w-8 rounded border-tertiaire_fonce border peer-checked:border-4"
+                        :style="{backgroundColor: context.option.value}"/>
+                <span class="sr-only">{{context.option.label}}</span>
+            </template>
+
+        </FormKit>
+    </div>
+</template>
