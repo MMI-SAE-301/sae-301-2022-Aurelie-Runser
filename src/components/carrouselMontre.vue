e8ffae20fed4ea490ad51147a3da6f4ea03f7d07
Aurélie R <aurelie.runser@edu.univ-fcomte.fr>
2022-10-26 21:00:46 +0200

    changement de nom du composant card_montre. vue -> cardMontre.vue pour une meilleur coeherence d'organisation

--- src/components/carrouselMontre.vue
+++ src/components/carrouselMontre.vue
@@ -1,13 +1,13 @@
 <template>
     <div class="my-10 py-5 carousel gap-x-5 w-full">
-        <card_montre class="carousel-item" image="montres/montre1.jpg"/>
-        <card_montre class="carousel-item" image="montres/montre2.jpg"/>
-        <card_montre class="carousel-item" image="montres/montre3.jpg"/>
-        <card_montre class="carousel-item" image="montres/montre4.jpg"/>
-        <card_montre class="carousel-item" image="montres/montre5.jpg"/>
+        <cardMontre class="carousel-item" image="montres/montre1.jpg"/>
+        <cardMontre class="carousel-item" image="montres/montre2.jpg"/>
+        <cardMontre class="carousel-item" image="montres/montre3.jpg"/>
+        <cardMontre class="carousel-item" image="montres/montre4.jpg"/>
+        <cardMontre class="carousel-item" image="montres/montre5.jpg"/>
     </div>
 </template>
 
 <script setup lang="ts">
-import card_montre from "@/components/card_montre.vue"
+import cardMontre from "@/components/cardMontre.vue"
 </script>
