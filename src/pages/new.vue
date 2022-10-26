1cea12cd00aa09a58903319013fa59a8b280f130
Aurélie R <aurelie.runser@edu.univ-fcomte.fr>
2022-10-26 16:43:31 +0200

    ajout du formulaire + ajout lien pour se connecter sur la page new.vue

--- src/pages/new.vue
+++ src/pages/new.vue
@@ -1,8 +1,28 @@
 <template>
-    <div class="px-5 md:px-20 pt-20 h-screen">
-        <h1>Personnalisation</h1>
+    <div class="px-5 md:px-20 pt-20">
+        <h1 class="text-lg sm:text-xl md:text-2xl">Personnalisation</h1>
 
+        <p class="my-10">Choisissez vous-même les couleurs et matériau de votre Tik●Tak .</p>
+
+        <!-- si l'utilisateur n'est pas connecté, on lui propose de le faire-->
+        <div v-if="!user" class="flex flex-wrap gap-10 justify-between items-center">
+            <p class="flex-auto basis-96 font-extrabold">Attention, vous devez être connecté(e) pour créer et commander une montre.</p>
+
+            <bouton_fonce class="mx-auto">
+                <RouterLink to="/comptes">
+                    Se connecter
+                </RouterLink>
+            </bouton_fonce>
+        </div>
+
+        <form_montre></form_montre>
         
 
     </div>
 </template>  
+
+<script setup lang="ts">
+import { user } from '@/supabase';
+import form_montre from "@/components/form_montre.vue"
+import bouton_fonce from "@/components/bouton_fonce.vue"
+</script>
