fc32b13ea3e2e7467db1d876af12fcb4fab99434
Aurélie R <aurelie.runser@edu.univ-fcomte.fr>
2022-10-26 19:52:11 +0200

    création page Montres.vue

---
+++ src/pages/montres.vue
@@ -0,0 +1,32 @@
+<template>
+    <div  class="pt-10">
+
+        <section>
+            <h1>Tik●Tak que vous avez créées</h1>
+
+            <listeMesMontres :max="3"/>
+        </section>
+
+        <section>
+            <h1>Tik●Tak créées par des clients</h1>
+
+            <listeMontres/>
+        </section>
+
+        <section>
+            <h1>Tik●Tak officielles</h1>
+
+            <listeMontres/>
+        </section>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { supabase, user } from '@/supabase';
+
+import bouton_fonce from "@/components/bouton_fonce.vue"
+import bouton_clair from "@/components/bouton_clair.vue"
+import listeMesMontres from "@/components/listeMesMontres.vue"
+import listeMontres from "@/components/listeMontres.vue"
+
+</script>
