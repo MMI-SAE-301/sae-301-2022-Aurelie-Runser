fb745970a2f844bad43f10abedbb32a9a86f1bac
Aurélie R <aurelie.runser@edu.univ-fcomte.fr>
2022-10-26 19:34:55 +0200

    ajout des tailles aux boutons pour qu'ils ne change pas de dimention

--- src/components/bouton_clair.vue
+++ src/components/bouton_clair.vue
@@ -1,5 +1,5 @@
 <template>
-    <button class="block rounded bg-secondaire_claire font-catamaran font-semibold text-tertiaire_fonce 
+    <button class="block h-max w-max rounded bg-secondaire_claire font-catamaran font-semibold text-tertiaire_fonce 
         px-5 py-2 text-base
         md:px-8 md:py-5 md:text-lg
         hover:bg-tonic">
--- src/components/bouton_fonce.vue
+++ src/components/bouton_fonce.vue
@@ -1,5 +1,5 @@
 <template>
-    <button class="block w-max rounded bg-secondaire_fonce font-catamaran font-semibold text-tertiaire_claire 
+    <button class="block h-max w-max rounded bg-secondaire_fonce font-catamaran font-semibold text-tertiaire_claire 
         px-5 py-2 text-base
         md:px-8 md:py-5 md:text-lg
         hover:bg-tonic">
